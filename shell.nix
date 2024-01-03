dd54ceb417a4b54e47553d41392984b44cf4a110
VedantWankhade <vedantwankhade17@gmail.com>
2024-01-03 18:34:52 +0530

    Nix shell - pin go version

---
+++ shell.nix
@@ -0,0 +1,24 @@
+let
+    nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+    pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+    packages = with pkgs; [
+        go
+        gotools
+        go-tools
+        mysql-shell
+    ];
+
+    shellHook = ''
+        echo "============= go-exercise ==================="
+        echo "Entered go-exercise environment."
+        echo "Dependencies"
+        go version
+        echo "------------------------"
+        echo "Git status"
+        git status
+    '';
+}
+
