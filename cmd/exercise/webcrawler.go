f8010b135b53a033c8b66b3dd50ae40d7f3066ce
VedantWankhade <vedantwankhade17@gmail.com>
2024-01-11 02:03:47 +0530

    functions as values

--- cmd/exercise/webcrawler.go
+++ cmd/exercise/webcrawler.go
@@ -7,6 +7,8 @@ import (
 	"golang.org/x/net/html"
 )
 
+var depth int
+
 func main() {
 	doc, err := html.Parse(os.Stdin)
 	if err != nil {
@@ -17,6 +19,33 @@ func main() {
 		fmt.Println(link)
 	}
 	outline(nil, doc)
+    forEachNode(doc, startElement, endElement)
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+    if pre != nil {
+        pre(n)
+    }
+    for c := n.FirstChild; c != nil; c = c.NextSibling {
+        forEachNode(c, pre, post)
+    }
+    if post != nil {
+        post(n)
+    }
+}
+
+func startElement(n *html.Node) {
+    if n.Type == html.ElementNode {
+        fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+        depth++
+    }
+}
+
+func endElement(n *html.Node) {
+    if n.Type == html.ElementNode {
+        depth--
+        fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+    }
 }
 
 func outline(stack []string, n *html.Node) {
