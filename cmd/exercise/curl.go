862b3fd1b4a9b54f117767276962ef65131f4ffc
VedantWankhade <vedantwankhade17@gmail.com>
2024-01-10 22:55:53 +0530

    recursion

--- cmd/exercise/curl.go
+++ cmd/exercise/curl.go
@@ -7,96 +7,97 @@ import (
 	"net/http"
 	"os"
 	"strings"
-    "time"
+	"time"
 )
 
 func main() {
-    parallelCurl()
+	// parallelCurl()
+
+	curl1()
 }
 
-func curl1() { 
-    for _, url := range os.Args[1:] {
-        res, err := http.Get(url)
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "curl: %v\n", err)
-            os.Exit(1)
-        }
+func curl1() {
+	for _, url := range os.Args[1:] {
+		res, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "curl: %v\n", err)
+			os.Exit(1)
+		}
 
-        byteData, err := ioutil.ReadAll(res.Body)
-        res.Body.Close()
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "curl: reading %s: %v\n", url, err)
-            os.Exit(1)
-        }
-        fmt.Printf("%s", byteData)
-    }
+		byteData, err := ioutil.ReadAll(res.Body)
+		res.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "curl: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", byteData)
+	}
 }
 
 // use io.Copy instead of ioutil.ReadAll
 func curl2() {
-    for _, url := range os.Args[1:] {
-        res, err := http.Get(url)
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "curl: %v\n", err)
-            os.Exit(1)
-        }
-        _, err = io.Copy(os.Stdout, res.Body)
-        res.Body.Close()
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "curl: writing os.Stdout from res.Body: %v", err)
-        }
-    }
+	for _, url := range os.Args[1:] {
+		res, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "curl: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, res.Body)
+		res.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "curl: writing os.Stdout from res.Body: %v", err)
+		}
+	}
 }
 
 // add https:// prefix it it doesnt exist
 func curl3() {
-   for _, url := range os.Args[1:] {
-       if !strings.HasPrefix(url, "https://") {
-            url = "https://" + url
-        }
-        res, err := http.Get(url)
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "curl: %v\n", err)
-            // os.Exit(1)
-            continue
-        }
-        fmt.Println("HTTP STATUS")
-        fmt.Println(res.Status)
-        _, err = io.Copy(os.Stdout, res.Body)
-        res.Body.Close()
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "curl: writing os.Stdout from res.Body: %v", err)
-        }
-    }
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "https://") {
+			url = "https://" + url
+		}
+		res, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "curl: %v\n", err)
+			// os.Exit(1)
+			continue
+		}
+		fmt.Println("HTTP STATUS")
+		fmt.Println(res.Status)
+		_, err = io.Copy(os.Stdout, res.Body)
+		res.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "curl: writing os.Stdout from res.Body: %v", err)
+		}
+	}
 }
-
 
 // parallelCurl fetches all urls concurrently
 func parallelCurl() {
-    start := time.Now()
-    ch := make(chan string)
-    for i, url := range os.Args[1:] {
-        go fetch(url, ch, i + 1)
-    }
-    for range os.Args[1:] {
-        fmt.Printf(<- ch)
-    }
-    fmt.Printf("Total time elapsed: %.2fs", time.Since(start).Seconds())
+	start := time.Now()
+	ch := make(chan string)
+	for i, url := range os.Args[1:] {
+		go fetch(url, ch, i+1)
+	}
+	for range os.Args[1:] {
+		fmt.Printf(<-ch)
+	}
+	fmt.Printf("Total time elapsed: %.2fs", time.Since(start).Seconds())
 }
 
-func fetch(url string, ch chan <- string, n int) {
-    start := time.Now()
-    fmt.Printf("thread %d started\n", n)
-    res, err := http.Get(url)
-    if err != nil {
-        ch <- fmt.Sprintf("%v\n", err)
-    } else {
-        nbytes, err := io.Copy(ioutil.Discard, res.Body)
-        res.Body.Close()
-        if err != nil {
-            ch <- fmt.Sprintf("error reading response: %v\n", err)
-        } else {
-            ch <- fmt.Sprintf("%.2fs\t%7d\t%s\n", time.Since(start).Seconds(), nbytes, url)
-        }
-    }
+func fetch(url string, ch chan<- string, n int) {
+	start := time.Now()
+	fmt.Printf("thread %d started\n", n)
+	res, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprintf("%v\n", err)
+	} else {
+		nbytes, err := io.Copy(ioutil.Discard, res.Body)
+		res.Body.Close()
+		if err != nil {
+			ch <- fmt.Sprintf("error reading response: %v\n", err)
+		} else {
+			ch <- fmt.Sprintf("%.2fs\t%7d\t%s\n", time.Since(start).Seconds(), nbytes, url)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/VedantWankhade/go-exercise
 
 go 1.21.5
+
+require golang.org/x/net v0.20.0 // indirect
